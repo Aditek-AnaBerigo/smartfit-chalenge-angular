e126ab0602e39fa9d55636e2775910421380c875
Ana Berigo <ana.berigo@aditek.com.br>
2023-10-17 14:00:19 -0300

    Adição do controle do formulário e corrigindo erros de responsividade.

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class FormsComponent {
   results = []
+  formGroup!: FormGroup
+
+  constructor(private formBuilder: FormBuilder){}
+
+  ngOnInit(){
+    this.formGroup = this.formBuilder.group({
+      hour: '',
+      showClosed: false
+    })
+  }
+
+  onSubmit(){
+    console.log(this.formGroup.value);
+
+  }
+
+  onClean(){
+    this.formGroup.reset()
+
+  }
 }
