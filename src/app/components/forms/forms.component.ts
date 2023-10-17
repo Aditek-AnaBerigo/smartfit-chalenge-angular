4ad842b925bce07cba61708598b1c9b26f9ed0b8
Ana Berigo <ana.berigo@aditek.com.br>
2023-10-17 14:28:38 -0300

    Criando o serviço get units.

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { GetUnitsService } from 'src/app/services/get-units.service';
 
 @Component({
   selector: 'app-forms',
@@ -10,9 +11,10 @@ export class FormsComponent {
   results = []
   formGroup!: FormGroup
 
-  constructor(private formBuilder: FormBuilder){}
+  constructor(private formBuilder: FormBuilder, private unitServices: GetUnitsService){}
 
   ngOnInit(){
+    this.unitServices.getAllUnits().subscribe(data => console.log(data))
     this.formGroup = this.formBuilder.group({
       hour: '',
       showClosed: false
